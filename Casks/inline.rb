1312cce66660af85878922f2883d314807e2ef28
Mohamad <mo.rajbi@gmail.com>
2026-01-17 17:54:18 +0330

    brew: update inline cask checksums

--- Casks/inline.rb
+++ Casks/inline.rb
@@ -2,8 +2,8 @@ cask "inline" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "0.2.3"
-  sha256 arm: "f42db8d2bbb65aece9de1b045fc7aba20de9fffc5eff8a1b999f1b3a69647292",
-         intel: "6000cbab999821d05897fc1278b801db4b09cb0ac95a132c8055899d3497f1b9"
+  sha256 arm: "31176daccb9909d74182d1287626c57a198e336b18a736fbd31d24ed50f0046f",
+         intel: "6970e09f0130602ba1debf6338bf0de8ef4fd3ec81529fd15c032588a7aaf398"
 
   url "https://github.com/inline-chat/inline/releases/download/cli-v#{version}/inline-cli-#{version}-#{arch}-apple-darwin.tar.gz"
   name "Inline CLI"
