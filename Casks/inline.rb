fd1385c1436bed7e3c75e5fb65faf68a87591a5f
Mohamad <mo@inline.chat>
2026-02-01 00:29:04 +0330

    cask: bump inline to v0.3.0

--- Casks/inline.rb
+++ Casks/inline.rb
@@ -1,9 +1,9 @@
 cask "inline" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2.4"
-  sha256 arm: "895ad004acc273e99922a5e729898904acb75f058fa382b177142674c6ea9bf9",
-         intel: "6bd229b55a8b7640b9714ee1a966bec7ba219273fb32869fde655efeefb17431"
+  version "0.3.0"
+  sha256 arm: "14621bd68f3c59b4b19cdd6936fefa78b608846e232b5abec7412d6138b70a21",
+         intel: "eaf8f0c1f03ed0bfdacd6203a8cdf5c666628868e092b98891eeb788f846b4e4"
 
   url "https://github.com/inline-chat/inline/releases/download/cli-v#{version}/inline-cli-#{version}-#{arch}-apple-darwin.tar.gz"
   name "Inline CLI"
