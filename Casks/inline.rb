d3c5a2b921e07d646f3d98307395da56a488f069
Mohammad Rajabi <mo.rajbi@gmail.com>
2026-08-28 01:46:06 +0330

    cask: bump inline to v0.7.5

--- Casks/inline.rb
+++ Casks/inline.rb
@@ -1,20 +1,20 @@
 cask "inline" do
-  version "0.7.4"
+  version "0.7.5"
   name "Inline CLI"
   desc "Command-line client for work chat"
   homepage "https://inline.chat/"
 
   on_macos do
     depends_on arch: :arm64
-    sha256 "cd9b2422ee3202446e599d5fa85ab45080e510ed729643fb6ad21bb6f930e943"
+    sha256 "fa8cbf3f60cfb585dfde420e4ebdbe9e48c631f4b7c0086c111311c981549e27"
     url "https://github.com/inline-chat/inline/releases/download/cli-v#{version}/inline-cli-#{version}-aarch64-apple-darwin.tar.gz",
         verified: "github.com/inline-chat/inline/"
   end
 
   on_linux do
     arch arm: "aarch64", intel: "x86_64"
-    sha256 arm64_linux:  "e9f57fe5b5f14e496341d9a69f7c8fd78990430207341de74f2fb2ff27918ec2",
-           x86_64_linux: "83fffbe7da27974b911c3391da39de3e4777720078fbb549085244be7d9fa463"
+    sha256 arm64_linux:  "a9f77994484b4be5f0637d052681e2a3b7c495519ace93f31c51854ef10a0de2",
+           x86_64_linux: "8f0524eba1d240a3b99242c9b6c02e0f98a2f06bf0e86f6df5e06d24352a0206"
     url "https://github.com/inline-chat/inline/releases/download/cli-v#{version}/inline-cli-#{version}-#{arch}-unknown-linux-gnu.tar.gz",
         verified: "github.com/inline-chat/inline/"
   end
